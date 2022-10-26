2565b2094b59c9a1dc178e71e64cf4d567b97b72
Yury Molaikow <molialow13@gmail.com>
2022-10-26 22:01:01 +0300

    feat: create reusable components project

---
+++ 08-reusable-components/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import MyComponent from './components/MyComponents';
+import NewComponent from './components/NewComponent';
+
+function App() {
+	return (
+		<div className="App">
+			<MyComponent />
+			<NewComponent />
+		</div>
+	);
+}
+
+export default App;
---
+++ 08-reusable-components/src/components/NewComponent.jsx
@@ -0,0 +1,9 @@
+function NewComponent() {
+	return (
+		<div>
+			<p>molialow13@gmail.com</p>
+		</div>
+	);
+}
+
+export default NewComponent;
