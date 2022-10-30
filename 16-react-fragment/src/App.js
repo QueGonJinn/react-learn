6465a1fc044c4f041f413cbdb19016204bd93718
Yury Molaikow <molialow13@gmail.com>
2022-10-30 15:06:47 +0300

    feat: create react fragment project

---
+++ 16-react-fragment/src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+import Card from './components/Card';
+function App() {
+	return (
+		<div className="App">
+			<Card />
+		</div>
+	);
+}
+
+export default App;
---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+	return (
+		<>
+			<h1>Yury</h1>
+			<h3>I'm developer</h3>
+			<button>Like!</button>
+		</>
+	);
+};
+
+export default Card;
